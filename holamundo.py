042cfc2aac03f4bc35d7626cb2199583d8f48fbd
Antonio Tian <lliurexantonio@gmail.com>
2023-02-20 12:01:51 +0100

    Primer commit

---
+++ holamundo.py
@@ -0,0 +1,12 @@
+
+
+print("Como te llamas")
+nom = input()
+print("Cúal es tu edat")
+edat = int(input())
+
+'''Operación'''
+resultado = input()
+resultado = 65 - edat
+print("Hola", nom, "te quedan ", resultado, "años para jubilarte")
+
